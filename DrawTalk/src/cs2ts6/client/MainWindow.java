aa690a212355c1d8e7863f3d659ff85a70cbd204
IanField90 <ian.field90@gmail.com>
2011-02-02 12:50:57 +0000

    Class establishment

---
+++ DrawTalk/src/cs2ts6/client/MainWindow.java
@@ -0,0 +1,24 @@
+package cs2ts6.client;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+//TODO 640x480 + 150 chat?
+public class MainWindow extends JPanel{
+
+	//TODO Canvas element within JPanel
+	
+	private static void createAndShowGUI(){
+		JFrame frame = new JFrame("Draw & Talk");
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println("hi")
+
+	}
+
+}
