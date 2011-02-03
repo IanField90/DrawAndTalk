f8025e450e61f56c21f4d3439eb2bf1ecde84c2c
IanField90 <ian.field90@gmail.com>
2011-02-03 16:41:41 +0000

    Add barebones GUI

---
+++ DrawTalk/src/cs2ts6/client/ChatPanel.java
@@ -0,0 +1,30 @@
+package cs2ts6.client;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+
+public class ChatPanel extends JPanel implements ActionListener{
+
+	/**
+	 * Automatically generated
+	 */
+	private static final long serialVersionUID = -2697225417654336509L;
+	private JPanel globalChat;
+	
+	public ChatPanel(){
+		JTabbedPane jtpChat = new JTabbedPane();
+		globalChat = new JPanel();
+		jtpChat.add("Global", globalChat);
+		//jtpChat.add()
+		add(jtpChat);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+}
--- DrawTalk/src/cs2ts6/client/MainWindow.java
+++ DrawTalk/src/cs2ts6/client/MainWindow.java
@@ -1,24 +1,44 @@
 package cs2ts6.client;
 
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
 //TODO 640x480 + 150 chat?
 public class MainWindow extends JPanel{
 
-	//TODO Canvas element within JPanel
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 983051938169997622L;
+	//TODO Drawing elements within JPanel
+	private DrawingPanel drawingPanel;
+	private ChatPanel chatPanel;
 	
-	private static void createAndShowGUI(){
+	private void createAndShowGUI(){
 		JFrame frame = new JFrame("Draw & Talk");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		drawingPanel = new DrawingPanel();
+		chatPanel = new ChatPanel();
+		frame.setPreferredSize(new Dimension(1000, 480));
+		frame.add("West", drawingPanel);
+		frame.add(chatPanel);
+		frame.pack();
+		frame.setVisible(true);
 	}
 	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//System.out.println("hi")
-
+		final MainWindow mainWindow = new MainWindow();
+		SwingUtilities.invokeLater(new Runnable(){
+			public void run(){
+				mainWindow.createAndShowGUI();
+			}
+		});
 	}
 
 }
