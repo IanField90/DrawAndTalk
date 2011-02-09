27c4d853fad57aca74e10f6acb8eb948998e0b6a
Stephen Simmons <stephen@simmonstech-main.(none)>
2011-02-09 16:17:42 +0000

    Added temp client2

--- DrawTalk/src/cs2ts6/client/Client.java
+++ DrawTalk/src/cs2ts6/client/Client.java
@@ -6,16 +6,26 @@ public class Client {
 	
 	private DrawingCanvas canvas;
 	private ChatPanel chat;
-	
+	/**
+	 * Construct the client class with refernces to canvas and chat windows
+	 * @param cnv Canvas reference
+	 * @param cht Chat window reference
+	 */
 	public Client(DrawingCanvas cnv, ChatPanel cht) {
 		canvas = cnv;
 		chat = cht;
 	}
-	
+	/**
+	 * Send point to server, will auto return message back in mock client
+	 * @param pnt
+	 */
 	public void sendPoints(PointPacket pnt) {
 		canvas.drawPoints(pnt);
 	}
-	
+	/**
+	 * Send message t server, will auto return to chat window for mock client
+	 * @param cht
+	 */
 	public void sendMessage(ChatPacket cht) {
 		chat.drawMessage(cht);
 	}
