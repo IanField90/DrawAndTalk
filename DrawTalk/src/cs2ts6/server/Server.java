2a1c76fb071275fe5c74de5853e5b6ce35c64ec1
Stephen Simmons <stephen@simmonstech-main.(none)>
2011-02-09 21:05:01 +0000

    WIP New Server Added

---
+++ DrawTalk/src/cs2ts6/server/Server.java
@@ -0,0 +1,27 @@
+package cs2ts6.server;
+
+import java.io.*;
+import java.net.*;
+
+public class Server {
+	private static final int port = 61000;
+	public static void main(String[] args) {
+		ServerSocket svrSkt = null;
+		
+		try {
+			svrSkt = new ServerSocket(port);
+		} catch (IOException e) {
+			System.err.println("Could not listen on port: "+port);
+			System.exit(-1);
+		}
+		
+		while (true) {
+			try {
+				new ServerThread(svrSkt.accept()).start();
+			} catch (IOException e) {
+				System.err.println("Error in client connection");
+				e.printStackTrace();
+			}
+		}
+	}
+}
---
+++ DrawTalk/src/cs2ts6/server/ServerThread.java
@@ -0,0 +1,15 @@
+package cs2ts6.server;
+
+import java.net.Socket;
+
+public class ServerThread extends Thread{
+	private Socket skt;
+	ServerThread(Socket connection) {
+		super();
+		skt = connection;
+	}
+	
+	public void run() {
+		
+	}
+}
